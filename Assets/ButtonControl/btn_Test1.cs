4577dc1059b85861f92d026c6939d41ec9c72e30
KeiLazu <kennixlazuardi@gmail.com>
2017-04-01 12:50:13 +0700

    initial commit

---
+++ Assets/ButtonControl/btn_Test1.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class btn_Test1 : MonoBehaviour {
+
+    public Text lbl_TestSubject;
+
+    protected string _FirstText = "First Text";
+    protected string _SecondText = "Second Text";
+
+    protected int i = 0;
+
+    private void Start()
+    {
+        
+    }
+
+    public void ChangeTextSubjectFirst()
+    {
+        i++;
+
+        lbl_TestSubject.text = _FirstText;
+
+        if (i>0)
+        {
+            Debug.Log(i.ToString());
+        }
+
+    }
+
+    public void ChangeTextSubjectSecond()
+    {
+
+        i++;
+
+        lbl_TestSubject.text = _SecondText;
+
+        if (i > 0)
+        {
+            Debug.Log(i.ToString());
+        }
+
+    }
+
+}
